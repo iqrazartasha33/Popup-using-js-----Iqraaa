e7108e983ef59df99c7a6fd2718353ab0372e6eb
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-10-24 16:00:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("openPopup");
+}
+
+function closePopup(){
+    popup.classList.remove("openPopup");
+}
